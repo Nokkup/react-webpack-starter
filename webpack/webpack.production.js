accf182a56ec0e1e112b2de5c8df47a29e825bf1
nokkup <kuprin.kirill.96@gmail.com>
2022-10-30 00:00:24 +0300

    feat: css minimizer

--- webpack/webpack.production.js
+++ webpack/webpack.production.js
@@ -1,3 +1,9 @@
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
 module.exports = {
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: ['...', new CssMinimizerPlugin()],
+  },
 }
